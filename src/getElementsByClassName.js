a75f20b952be6837a0fa8c0290ed2561beca3fa7
Hack Reactor Student <student@hackreactor.com>
2017-03-27 15:44:46 -0700

    Complete getElementsByClassName and stringifyJSON

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,22 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className, element) {
+
+	var results = [];
+	element = element || document.body;
+
+
+		if ( element.classList && element.classList.contains(className)) {
+			results.push(element);
+		}
+
+		if ( element.hasChildNodes() ) {
+
+			element.childNodes.forEach(function(node){
+				results = results.concat( getElementsByClassName(className, node) );
+			});
+		}
+
+	return results;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,53 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+	var result = ''
+
+ 	if (typeof obj === 'number' || typeof obj === 'boolean') {
+
+  		result += String(obj);
+
+ 	} else if (obj === null) {
+
+ 		result += 'null';
+
+ 	} else if (typeof obj === 'string' ) {
+
+ 		result += ('"' + obj + '"' );
+
+ 	} else if ( typeof obj === 'object' ){
+
+ 		if (Array.isArray(obj)) {
+
+	 		result += '[';
+	 		obj.forEach(function(ele, idx) {
+	 			if (idx === 0) {
+	 				result += stringifyJSON(ele);
+	 			} else {
+	 				result += ',' + stringifyJSON(ele);
+	 			}
+	 		})
+	 		result += ']';
+
+	 	} else if ( Object.keys(obj) ){
+
+	 		result += "{";
+	 		Object.keys(obj).forEach(function(key, index){
+
+	 			if ( typeof obj[key] !== "function" && typeof obj[key] !== 'undefined' ){
+
+		 			if ( index === 0 ){
+						result += ( stringifyJSON(key) +":"+ stringifyJSON( obj[key] ) );
+		 			} else {
+		 				result += ( ","+stringifyJSON(key)+":"+stringifyJSON(obj[key] ) );
+		 			}
+		 		}
+
+	 		});
+	 		result += "}";
+
+	 	} 
+
+ 	}
+ 	return result;
 };
